c471c104838e179c25baf624b7a2f4388d0e97c5
L4ravel <usmanirawan00@gmail.com>
2025-11-04 16:48:19 +0800

    Add files via upload

--- app/admin/verifikasi-ujian/page.js
+++ app/admin/verifikasi-ujian/page.js
@@ -390,7 +390,7 @@ export default function VerifikasiUjian() {
   "3) Tes Wawancara.",
   "4) Pengukuran baju/seragam.",
   "",
-  "Mohon hadir tepat waktu dan berpakaian rapi. Disarankan datang 10–15 menit lebih awal.",
+  "Peserta wajib hadir bersama wali yang sesuai (siswa putra dengan wali putra, siswa putri dengan wali putri), tepat waktu, dan mengenakan pakaian rapi. Dianjurkan tiba 10–15 menit lebih awal.",
   "(Catatan : Bagi yang berada di luar daerah, silakan konfirmasi kepada panitia untuk pelaksanaan ujian secara online.)",
   "",
   "Jazakallah khairan.",
